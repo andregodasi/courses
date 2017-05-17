d7dbce59942689764882aec6959c931d5b56a888
andregodasi <andre.godasi@gmail.com>
2017-05-17 17:44:30 -0300

    aula 4,5 e 6

---
+++ angular_2_4/04_introdução_ao_typescript_para_angular/workout/main.ts
@@ -0,0 +1,17 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunc(y,x){
+
+    return x + y;
+}
+
+let num = 0;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+
+numeros.map(function(valor){
+    return valor * 2;
+});
+
+numeros.map(valor => valor * 2);
